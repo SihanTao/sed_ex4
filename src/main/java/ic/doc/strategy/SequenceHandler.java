16d51a195d25d7835b7863550e457d99b6a6fffc
st2220 <sihan.tao@imperial.ac.uk>
2021-10-26 17:16:54 +0100

    [Sihan Tao, Lynna Xie] Use Google Java Format.

--- src/main/java/ic/doc/strategy/SequenceHandler.java
+++ src/main/java/ic/doc/strategy/SequenceHandler.java
@@ -3,11 +3,12 @@ package ic.doc.strategy;
 import java.util.Iterator;
 
 public interface SequenceHandler {
-    default int term(int i) {
-        if (i < 0) {
-            throw new IllegalArgumentException("Not defined for indices < 0");
-        }
-        return calculateTerm(i);
+  default int term(int i) {
+    if (i < 0) {
+      throw new IllegalArgumentException("Not defined for indices < 0");
     }
-    int calculateTerm(int i);
+    return calculateTerm(i);
+  }
+
+  int calculateTerm(int i);
 }
