a47d3285aa2c4edd713e361d834956c6c9107861
st2220 <sihan.tao@imperial.ac.uk>
2021-10-26 16:10:25 +0100

    [Sihan Tao, Lynna Xie] Implement and pass the test for definesFirstTermToBeOne

--- src/test/java/ic/doc/templatemethod/TriangleNumberSequenceTest.java
+++ src/test/java/ic/doc/templatemethod/TriangleNumberSequenceTest.java
@@ -11,37 +11,34 @@ import static org.junit.Assert.fail;
 
 public class TriangleNumberSequenceTest {
 
-    final ic.doc.strategy.FibonacciSequence sequence = new FibonacciSequence();
+  final ic.doc.strategy.FibonacciSequence sequence = new FibonacciSequence();
 
-    @Test
-    public void definesFirstTwoTermsToBeOne() {
+  @Test
+  public void definesFirstTermToBeOne() {
+    assertThat(sequence.term(0), is(1));
+  }
 
-        assertThat(sequence.term(0), is(1));
-        assertThat(sequence.term(1), is(1));
+  @Test
+  public void definesSubsequentTermsToBeTheSumOfThePreviousTwo() {
+
+    assertThat(sequence.term(2), is(2));
+    assertThat(sequence.term(3), is(3));
+    assertThat(sequence.term(4), is(5));
+  }
+
+  @Test
+  public void isUndefinedForNegativeIndices() {
+
+    try {
+      sequence.term(-1);
+      fail("should have thrown exception");
+    } catch (IllegalArgumentException e) {
+      assertThat(e.getMessage(), containsString("Not defined for indices < 0"));
     }
+  }
 
-    @Test
-    public void definesSubsequentTermsToBeTheSumOfThePreviousTwo() {
-
-        assertThat(sequence.term(2), is(2));
-        assertThat(sequence.term(3), is(3));
-        assertThat(sequence.term(4), is(5));
-    }
-
-    @Test
-    public void isUndefinedForNegativeIndices() {
-
-        try {
-            sequence.term(-1);
-            fail("should have thrown exception");
-        } catch (IllegalArgumentException e) {
-            assertThat(e.getMessage(), containsString("Not defined for indices < 0"));
-        }
-    }
-
-    @Test
-    public void canBeIteratedThrough() {
-        assertThat(sequence, beginsWith(1, 1, 2, 3, 5));
-    }
-
+  @Test
+  public void canBeIteratedThrough() {
+    assertThat(sequence, beginsWith(1, 1, 2, 3, 5));
+  }
 }
